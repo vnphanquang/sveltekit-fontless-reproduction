312890970458df12dfbf837f1e150b52a59e1a55
Quang Phan <vnphanquang@gmail.com>
2025-06-12 22:58:05 +0700

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<main>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</main>
+
+<style>
+	main {
+		font-family: Lora, sans-serif;
+	}
+</style>
